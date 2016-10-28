22ff55852f340199c5a416475a8062d95a871012
virginiachae <chae.virginia@gmail.com>
2016-10-28 15:15:59 -0700

    finished mvp of game

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
 // wait for the DOM to finish loading
+console.log('linked!');
+
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  var clickCount = 0;
+  var $boxes = $('div.box');
+  //reset button
+  $('button').on('click', reset);
+  //console.log($boxes);
+  //sanity check click!
+  // $boxes.on('click', function consoleClicked() {
+  //   console.log('clicked!');
+  // });
+  $boxes.one('click', markBox);
 
+  function markBox() {
+    clickCount += 1;
+    console.log(this);
+    if(clickCount%2 === 0) {
+      var currentBox = this;
+      $(this).text('O')
+      $(this).text('O')
+      console.log('O turn');
+    } else {
+      $(this).text('X')
+      console.log('X turn');
+    }
+  //checkWinner();
+  }
+  function reset() {
+    clickCount = 0;
+    $boxes.text('');
+    $boxes.off('click');
+    $boxes.one('click', markBox);
+  }
 });
+
+function checkWinner() {
+  console.log('hello');
+}
