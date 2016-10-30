d17c26202ce80adf53523cdc578f016d10772bab
virginiachae <chae.virginia@gmail.com>
2016-10-29 20:10:22 -0700

    added css styling for each X and O and added whose turn it is

--- js/app.js
+++ js/app.js
@@ -8,35 +8,57 @@ $(document).ready(function() {
   var $boxes = $('div.box');
   //reset button
   $('button').on('click', reset);
-  //console.log($boxes);
-  //sanity check click!
-  // $boxes.on('click', function consoleClicked() {
-  //   console.log('clicked!');
-  // });
+  //text on top of page
+  $('hr').after("<p>Let's play! <span>X will start!</span></p>")
+  //clicking actions
   $boxes.one('click', markBox);
-
   function markBox() {
     clickCount += 1;
-    console.log(this);
     if(clickCount%2 === 0) {
-      var currentBox = this;
+      $('p').text("X's TURN");
       $(this).text('O')
-      $(this).text('O')
-      console.log('O turn');
+      $(this).css("color", "orange");
+      $('p').css("color", "pink");
+      checkWinner()
     } else {
+      $('p').text("O's TURN");
       $(this).text('X')
-      console.log('X turn');
+      $(this).css("color", "pink");
+      $('p').css("color", "orange");
+      checkWinner();
     }
-  //checkWinner();
   }
   function reset() {
     clickCount = 0;
+    $('p').html("<p>Let's play! <span>X will start!</span></p>");
     $boxes.text('');
     $boxes.off('click');
     $boxes.one('click', markBox);
   }
 });
 
-function checkWinner() {
-  console.log('hello');
-}
+// function checkWinner() {
+// if (
+//   $('#one').text() && $('#two').text() && $('#three').text() === 'X'||
+//   $('#one').text() && $('#four').text() && $('#seven').text() === 'X' ||
+//   $('#one').text() && $('#five').text() && $('#nine').text() === 'X' ||
+//   $('#two').text() && $('#five').text() && $('#eight').text() === 'X' ||
+//   $('#three').text() && $('#six').text() && $('#nine').text() === 'X' ||
+//   $('#four').text() && $('#five').text() && $('#six').text() === 'X' ||
+//   $('#seven').text() && $('#eight').text() && $('#nine').text() === 'X'
+// ) {
+//   $('p').text("X WINS!");
+//   $('p').css("color", "pink");
+// } else if (
+//   $('#one').text() && $('#two').text() && $('#three').text() === 'O'||
+//   $('#one').text() && $('#four').text() && $('#seven').text() === 'O' ||
+//   $('#one').text() && $('#five').text() && $('#nine').text() === 'O' ||
+//   $('#two').text() && $('#five').text() && $('#eight').text() === 'O' ||
+//   $('#three').text() && $('#six').text() && $('#nine').text() === 'O' ||
+//   $('#four').text() && $('#five').text() && $('#six').text() === 'O' ||
+//   $('#seven').text() && $('#eight').text() && $('#nine').text() === 'O'
+// ) {
+//   $('p').text("O WINS!");
+//   $('p').css("color", "orange");
+// }
+// };
